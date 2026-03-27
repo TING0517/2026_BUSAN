643d97ffb8489f57d7442bb84318d5b5a945569c
TING0517 <a2810030@gmail.com>
2026-03-28 01:04:12 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const CACHE_NAME = 'busan-travel-v1';
+const CACHE_NAME = 'busan-travel-v1.2'; // 每次更新程式碼後，請修改此版本號
+
 const ASSETS = [
+  './',
   'index.html',
   'guide.html',
   'wallet.html',
@@ -9,10 +11,29 @@ const ASSETS = [
   'icon.png'
 ];
 
+// 安裝時立即接管
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('正在快取資源...');
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 激活時清理舊的快取資料
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('正在清理舊快取:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
